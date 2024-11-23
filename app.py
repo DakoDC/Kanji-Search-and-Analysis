ad2b1dc325504027a8ef5be49fed04c7fbd3f702
Dakota Calori <dakota.calori@gmail.com>
2024-11-24 00:31:49 +0100

    com3, improvements on readability and charts

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 
+
+
 import json                 # read json file
 import polars as pl
 import streamlit as st
@@ -8,21 +10,26 @@ import os                   # cehck if file exists
 # url = https://github.com/davidluzgouveia/kanji-data
 
 kanji_url = r'C:\Users\dakot\OneDrive\Desktop\Git-repositories\Kanji-Analysis\kanji.json'
-with open(kanji_url, 'r', encoding="UTF-8" ) as file:
-    data = json.load(file)
+st.set_page_config(layout="wide")
 
-## print( pl.read_json(kanji_json).head()) # formato del file non adatto per read_json
+@st.cache_data
+def kanji_load():
+    with open(kanji_url, 'r', encoding="UTF-8" ) as file:
+        data = json.load(file)
+    ## print( pl.read_json(kanji_json).head()) # formato del file non adatto per read_json
+    kanjis_raw = list()
+    for kanji in data: # for every kanji in the file, each with its data
+        kanji_dict = {"save": False,"character": kanji} # convert the kanji to an item in a dict
+        kanji_dict.update(data[kanji]) 
+        kanjis_raw.append(kanji_dict) # append the new dict of the kanji
+    
+    # Create a Polars DataFrame from the list of dictionaries, with only the useful columns
+    # kanjis = pl.DataFrame(kanjis_raw).select(
+    #     pl.col(["character", "meanings","readings_on","wk_meanings", "wk_radicals"])
+    #     )
+    return pl.DataFrame(kanjis_raw) # , kanjis
 
-kanjis_raw = list()
-for kanji in data: # for every kanji in the file, each with its data
-    kanji_dict = {"character": kanji} # convert the kanji to an item in a dict
-    kanji_dict.update(data[kanji]) 
-    kanjis_raw.append(kanji_dict) # append the new dict of the kanji
-
-# Create a Polars DataFrame from the list of dictionaries, with only the useful columns
-kanjis = pl.DataFrame(kanjis_raw).select(
-    pl.col(["character", "meanings","readings_on","wk_meanings", "wk_radicals"])
-    )
+kanjis = kanji_load()
 
 # '''print("Total number of kanjis in the df: ",kanjis.select(pl.count("character")))
 # 13108
@@ -45,9 +52,6 @@ kanjis = pl.DataFrame(kanjis_raw).select(
 
 
 
-
-
-
 # Note: Some of the meanings and readings that were extracted from WaniKani have a ^ or a ! prefix.
 # I added these to denote when an item is not a primary answer (^) or not an accepted answer (!) on WaniKani. 
 
@@ -58,20 +62,24 @@ def kanji_search(word_search):
     # The dataframe's english words are formatted as capitalized
     word_search = word_search.lower().capitalize()
 
-    # a double confirmation is necessary since in the df a single kanji has a primary meaning and different secondary meanings
-    df = kanjis.filter(
-        # check if the word is in the "meanings" column
-        pl.col("meanings").list.contains(word_search)
-
-        &
-
-        # check if the word is in the "wk_meanings" column
-        (  
-            pl.col("wk_meanings").list.contains(word_search)
-            |
-            pl.col("wk_meanings").list.contains(f"^{word_search}") # easier then removing the '^' from the words in the df
-        )
+    # check if the word is in the "meanings" column
+    meanings = pl.col("meanings").list.contains(word_search)
+    
+    # check if the word is in the "wk_meanings" column
+    wk_meanings = (
+        pl.col("wk_meanings").list.contains(word_search)
+        |
+        pl.col("wk_meanings").list.contains(f"^{word_search}") # easier then removing the '^' from the words in the df
     )
+
+    # Gets the word/s with the clear desired meaning
+    df = kanjis.filter(meanings & wk_meanings)
+    # A double confirmation is necessary since in the df a single kanji has a primary meaning and different secondary meanings
+
+    # Gets the word/s with a less clear desired meaning
+    if df.is_empty():
+        df = kanjis.filter(meanings | wk_meanings)
+
     return df
     
 
@@ -84,19 +92,17 @@ def kanji_search(word_search):
 
 ############# Streamlit ###############
 
-# file with the saved kanjis library
-saved_url = r"C:\Users\dakot\OneDrive\Desktop\Git-repositories\Kanji-Analysis\saved.tsv"
-
 # Title
 st.write('# Kanji search')
 
 # User chooses the word to search and return the df with the corresponding kanjis
 search_word = st.text_input("Insert word to search: ")
-search_df = kanji_search(search_word).with_columns(
-    save = pl.lit(False) # add a save column, to save the kanji in the library
-)
-
+search_df = kanji_search(search_word)
+# .with_columns(
+#     save = pl.lit(False) # add a save column, to save the kanji in the library
+# )
 # create a checkbox in the save column
+
 search_df_edit = pl.DataFrame(st.data_editor( # returns a pandas df
     search_df,
     column_config={
@@ -105,29 +111,44 @@ search_df_edit = pl.DataFrame(st.data_editor( # returns a pandas df
             default=False # checkbox is initially not checked
         )
     },
+    use_container_width=True,
+    column_order=("save","character","meanings","wk_meanings", "readings_on","readings_kun","wk_radicals"),
     disabled=["character", "meanings","readings_on","wk_meanings", "wk_radicals"]
 ))
 
 
 
 
-#######################
+# file with the saved kanjis library
+saved_url = r"C:\Users\dakot\OneDrive\Desktop\Git-repositories\Kanji-Analysis\saved.tsv"
 
 # if the file doesn't exists it initializes it with a kanji as example
+# st.write(kanjis.columns)
+kanjis_col_all = 'save'
+for i in kanjis.columns[2:]:
+    kanjis_col_all = kanjis_col_all +'\t' + i 
+
+# a = pd.DataFrame(kanjis.head(1))
+# a.insert(loc = 0,column='save',value=True)
+# st.write(a)
+
 def initialize_file():
     if not os.path.exists(saved_url):
         row0 = pd.DataFrame(kanjis.head(1)) # example kanji df
-        row0['save'] = True # add the save column to it
+        row0['save'] = True
+        # row0['save'] = True # add the save column to it
+        # st.write(row0)
 
         # creates the library file and writes the header
         with open(saved_url, mode="w") as f:
-            f.write('character\tmeanings\treadings_on\twk_meanings\twk_radicals\tsave\n')
+            f.write(kanjis_col_all + "\n")
         
         # append the example dataframe
         row0.to_csv(saved_url, mode="a", sep="\t" ,header=False, index=False)
 
 # Initialize the file if missing
-initialize_file()
+if not os.path.exists(saved_url):
+    initialize_file()
 
 # reads the library file
 saved = pl.read_csv(saved_url, separator='\t',truncate_ragged_lines=True)
@@ -144,7 +165,9 @@ saved_edit = pl.DataFrame(st.data_editor( # returns a pandas df
             # search dataframe earlier, so the checkbox is checked by default
         )
     },
-    disabled=["character", "meanings","readings_on","wk_meanings", "wk_radicals"]
+    use_container_width=True,
+    column_order=("save","character","meanings","wk_meanings", "readings_on","readings_kun","wk_radicals"),
+    disabled=("character","meanings","wk_meanings", "readings_on","readings_kun","wk_radicals")
 ))
 
 # df with the kanjis checked in the search df, that need to be added to the library
@@ -175,3 +198,66 @@ if not (saved_edit["save"].all()):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+############################
+
+# Polars
+
+print(
+    kanjis
+    .group_by()
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+#########
+
+# Analysis
+
+import altair as alt
+
+# strokes ~ jlpt
+# st.write(kanjis)
+
+chart = (
+    alt.Chart(kanjis)
+    .mark_point()
+    .encode(
+        alt.X("strokes"),
+        alt.Y("freq"),
+        alt.Color("jlpt_new")
+    )
+         
+)
+
+st.altair_chart(chart, use_container_width=True)
+
+radicals = kanjis["wk_radicals"].explode()
+# print(pl.DataFrame(radicals))
+# most used radicals
+print(
+    pl.DataFrame(radicals).
+    select(pl.col("wk_radicals").value_counts())
+    .unnest("wk_radicals")
+    .sort(by="count",descending=True)
+)
